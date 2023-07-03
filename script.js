ff43997ff5c3e289922c6b6dfc25d202635d4258
Gaurav_is_the_one <76547705+gauravkm59829@users.noreply.github.com>
2023-07-03 12:52:34 +0530

    Update script.js
    Added some JS code to perform copy operation of Hex Code.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const copyButton = document.getElementById('copyButton');
 const getColor = () => {
    const randomNumber = Math.floor(Math.random() * 16777215);
    const randomCode = "#" + randomNumber.toString(16);
@@ -5,6 +6,17 @@ const getColor = () => {
    document.getElementById("code").innerHTML = randomCode;
    navigator.clipboard.writeText(randomCode);
 }
+copyButton.addEventListener('click', () => {
+   const hexCode = code.textContent.trim();
+   const tempInput = document.createElement('input');
+   document.body.appendChild(tempInput);
+   tempInput.value = hexCode;
+   tempInput.select();
+   document.execCommand('copy');
+   document.body.removeChild(tempInput);
+   // Additional code to show a success message or perform other actions after the content is copied
+   alert("Hex Code is copied.✔")
+ });
 
 // event call
 document.getElementById("btn").addEventListener("click",getColor);
